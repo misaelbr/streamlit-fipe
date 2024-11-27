68e2dcbd381261a3442ec99565d59376e957d64a
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-11-27 01:51:54 -0300

    feat: create history prices

--- app/repository.py
+++ app/repository.py
@@ -1,5 +1,6 @@
 from app.services import fetch_marcas, fetch_modelos, fetch_anos, fetch_detalhes_ano, fetch_tabelas
 from app.models import Marca, Modelo, Ano, DetalhesVeiculo, Tabela
+import pandas as pd
 
 def get_marcas() -> list[Marca]:
     data = fetch_marcas()
@@ -19,4 +20,27 @@ def get_detalhes_ano(codigo_marca: str, codigo_modelo: str, codigo_ano: str, mes
 
 def get_tabelas() -> list[Tabela]:
     data = fetch_tabelas()
-    return [Tabela(item) for item in data]
+    
+    
+    tabelas = [Tabela(item) for item in data]
+    
+    
+    return tabelas
+
+def get_variacao_preco(codigo_marca: str, codigo_modelo: str, codigo_ano: str, tabela_referencia: list[Tabela]) ->pd.DataFrame:
+    
+    precos = []
+
+    for tabela in tabela_referencia:
+        detalhes = get_detalhes_ano(codigo_marca, codigo_modelo, codigo_ano, tabela.codigo)        
+        preco = detalhes.valor
+        if preco is not None:
+            precos.append({"mes_referencia": tabela.mes, "preco": preco})
+            
+    precos.reverse()
+    
+    df = pd.DataFrame(precos)
+    df["preco"] = df["preco"].str.replace("R$", "").str.replace(".", "").str.replace(",", ".").astype(float)
+    
+    return df
+    
