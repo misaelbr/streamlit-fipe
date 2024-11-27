506b51f15b536dc324a2eb2e0e23e86c595c7178
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-11-27 01:53:14 -0300

    chore: package update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 # Reexportando os módulos principais
 from .services import fetch_marcas, fetch_modelos, fetch_anos, fetch_detalhes_ano, fetch_tabelas
-from .repository import get_marcas, get_modelos, get_anos, get_detalhes_ano, get_tabelas
+from .repository import get_marcas, get_modelos, get_anos, get_detalhes_ano, get_tabelas, get_variacao_preco
 from .models import Marca, Modelo, Ano, DetalhesVeiculo, Tabela
