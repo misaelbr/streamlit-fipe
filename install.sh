8d4e645a488de0f9e0bdeb74837bb0db93c269d2
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-11-27 19:34:00 -0300

    chore: formating bash script with colors

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# Definir cores
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Sem cor
+
 # Nome do arquivo .env a ser criado
 ENV_FILE=".env"
 ENV_DIST_FILE=".env-dist"
 
+echo -e "${CYAN}Iniciando configuração do ambiente...${NC}"
+
 # Verifica se o arquivo .env já existe
 if [[ -f $ENV_FILE ]]; then
-    echo "$ENV_FILE já existe. Não será sobrescrito."
+    echo -e "${YELLOW}$ENV_FILE já existe. Não será sobrescrito.${NC}"
 else
     # Copia o .env-dist para criar o .env
     if [[ -f $ENV_DIST_FILE ]]; then
         cp $ENV_DIST_FILE $ENV_FILE
-        echo "$ENV_FILE foi criado a partir de $ENV_DIST_FILE."
+        echo -e "${GREEN}$ENV_FILE foi criado a partir de $ENV_DIST_FILE.${NC}"
     else
-        echo "Erro: O arquivo $ENV_DIST_FILE não foi encontrado."
+        echo -e "${RED}Erro: O arquivo $ENV_DIST_FILE não foi encontrado.${NC}"
         exit 1
     fi
 fi
@@ -22,25 +32,38 @@ fi
 VENV_DIR=".venv"
 
 if [[ -d $VENV_DIR ]]; then
-    echo "O ambiente virtual $VENV_DIR já existe."
+    echo -e "${YELLOW}O ambiente virtual $VENV_DIR já existe.${NC}"
 else
     python3 -m venv $VENV_DIR
-    echo "Ambiente virtual $VENV_DIR criado."
+    if [[ $? -eq 0 ]]; then
+        echo -e "${GREEN}Ambiente virtual $VENV_DIR criado com sucesso.${NC}"
+    else
+        echo -e "${RED}Erro ao criar o ambiente virtual $VENV_DIR.${NC}"
+        exit 1
+    fi
 fi
 
 # Ativar o ambiente virtual
+echo -e "${CYAN}Ativando o ambiente virtual...${NC}"
 source $VENV_DIR/bin/activate
 
 # Instalar as dependências
 if [[ -f "requirements.txt" ]]; then
+    echo -e "${CYAN}Instalando dependências a partir de requirements.txt...${NC}"
     pip install --upgrade pip
     pip install -r requirements.txt
-    echo "As dependências foram instaladas."
+    if [[ $? -eq 0 ]]; then
+        echo -e "${GREEN}As dependências foram instaladas com sucesso.${NC}"
+    else
+        echo -e "${RED}Erro ao instalar as dependências.${NC}"
+        deactivate
+        exit 1
+    fi
 else
-    echo "Erro: O arquivo requirements.txt não foi encontrado."
+    echo -e "${RED}Erro: O arquivo requirements.txt não foi encontrado.${NC}"
     deactivate
     exit 1
 fi
 
 # Finaliza o script
-echo "Configuração concluída. Ambiente virtual configurado e dependências instaladas."
+echo -e "${BLUE}Configuração concluída.${NC} ${GREEN}Ambiente virtual configurado e dependências instaladas.${NC}"
